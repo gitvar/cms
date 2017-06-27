24c1c1be6bac5505cc6ebd423476d17e7135db49
gitvar <benvanderm@gmail.com>
2017-06-27 11:43:51 +0200

    Add text file viewing capability - Lesson 4

--- cms.rb
+++ cms.rb
@@ -9,13 +9,24 @@ get "/" do
   # Find all files in the data directory:
   @files = Dir.glob(root + "/data/*").select do |file|
     File.ftype(file) == "file"
-  end
+  end.sort
 
   # Remove prepended directory names:
   @files.map! { |file| File.basename(file) }.sort
 
-  # @files = Dir.glob(root + "/data/*").map do |path|
-  #   File.basename(path)
-  # end
+#   get "/" do
+#   @files = Dir.glob(root + "/data/*").map do |path|
+#     File.basename(path)
+#   end
+#   erb :index
+# end
+
   erb :index
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
