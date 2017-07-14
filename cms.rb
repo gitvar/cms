9797a82c85a086185489c61a7e3cb1b42db36e33
gitvar <benvanderm@gmail.com>
2017-07-14 21:09:46 +0200

    Completed lessons - fine tuning

--- cms.rb
+++ cms.rb
@@ -3,6 +3,9 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "redcarpet"
 require "yaml"
+require "bcrypt"
+
+VALID_EXTENTIONS = [".md", ".txt"].freeze
 
 configure do
   enable :sessions
@@ -17,7 +20,18 @@ def data_path
   end
 end
 
+def puts_this(message)
+  puts message
+end
+
 helpers do
+
+  class Stream
+    def each
+      100.times { |i| yield "#{i}\n" }
+    end
+  end
+
   def load_user_credentials
     credentials_path = if ENV["RACK_ENV"] == "test"
       File.expand_path("../test/users.yml", __FILE__)
@@ -25,6 +39,23 @@ helpers do
       File.expand_path("../users.yml", __FILE__)
     end
     YAML.load_file(credentials_path)
+  end
+
+  def valid_credentials?(username, password)
+    credentials = load_user_credentials
+
+    if credentials.has_key?(username)
+      # bcrypt_decrypted_password = BCrypt::Password.new(credentials[username])
+
+      # bcrypt_decrypted_password == password
+      # The line below is the same as the one directly above.
+      BCrypt::Password.new(credentials[username]).is_password?(password)
+
+      # password == bcrypt_decrypted_password # This does not work, because
+      # BCrypt::Password overrides the == ... See docs.
+    else
+      false
+    end
   end
 
   def render_markdown(markdown_text)
@@ -38,7 +69,7 @@ helpers do
     case File.extname(file_path)
     when ".md"
       erb render_markdown(file_content)
-    else
+    when ".txt"
       headers["Content-Type"] = "text/plain"
       file_content # Notice how text files are not displayed within the layout because the erb method isn't called for them.
     end
@@ -54,21 +85,32 @@ helpers do
       redirect "/"
     end
   end
+
+  def valid_filename?(filename)
+    VALID_EXTENTIONS.include?(File.extname(filename))
+  end
+
+  def files_array
+    # Find all files in the data directory:
+    pattern = File.join(data_path, "*")
+
+    files = Dir.glob(pattern).select do |file|
+      File.ftype(file) == "file"
+    end
+
+    # Remove prepended directory names and any sole directory names:
+    files.map! { |file| File.basename(file) }.sort
+  end
 end
 
 get "/" do
-  # Find all files in the data directory:
-  pattern = File.join(data_path, "*")
-
-  @files = Dir.glob(pattern).select do |file|
-    File.ftype(file) == "file"
-  end
-
-  # Remove prepended directory names and any sole directory names:
-  @files.map! { |file| File.basename(file) }.sort
+  @files = files_array
 
   erb :index
 end
+
+#  Delete this later!
+get('/123') { Stream.new }
 
 get "/new" do
   require_signed_in_user
@@ -77,9 +119,11 @@ get "/new" do
 end
 
 get "/:filename" do
-  file_path = File.join(data_path, params[:filename])
+  # get "/view" do # Used to illustrate Security in the course.
+  file_path = File.join(data_path, File.basename(params[:filename]))
 
-  if File.file?(file_path)
+  # file_path = File.join(data_path, params[:filename])
+  if File.exist?(file_path)
     load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
@@ -113,17 +157,25 @@ end
 post "/create" do
   require_signed_in_user
 
-  filename = params[:filename].to_s # Makes the line below work correctly.
+  filename = params[:filename].to_s.downcase # Convert to String so the line which checks for zero length filenames works correctly.
 
   if filename.size == 0
     session[:message] = "A name is required."
+    status 422
+    erb :new
+  elsif !valid_filename?(filename)
+    session[:message] = "#{filename} does not have a valid file extention."
+    status 422
+    erb :new
+  elsif files_array.include?(filename)
+    session[:message] = "The file #{filename} already exists."
     status 422
     erb :new
   else
     file_path = File.join(data_path, filename)
 
     File.write(file_path, "")
-    session[:message] = "#{params[:filename]} has been created."
+    session[:message] = "#{filename} has been created."
 
     redirect "/"
   end
@@ -144,27 +196,14 @@ get "/users/signin" do
   erb :signin
 end
 
-# post "/users/signin" do
-#   username = params[:username].to_s
-#   password = params[:password].to_s
-#
-#   if username == "admin" && password == "secret"
-#     session[:username] = params[:username]
-#     session[:message] = "Welcome!"
-#     redirect "/"
-#   else
-#     session[:message] = "Invalid Credentials"
-#     redirect "/users/signin"
-#   end
-# end
-
 post "/users/signin" do
-  credentials = load_user_credentials
+  # credentials = load_user_credentials
   username = params[:username]
 
-  if credentials.key?(username) && credentials[username] == params[:password]
-    # if params[:username] == "admin" && params[:password] == "secret"
-    session[:username] = params[:username]
+  # if credentials.key?(username) && credentials[username] == params[:password]
+  # if params[:username] == "admin" && params[:password] == "secret"
+  if valid_credentials?(username, params[:password])
+    session[:username] = username
     session[:message] = "Welcome!"
     redirect "/"
   else
--- data/New_1.md
+++
@@ -1,9 +0,0 @@
-# Heading 1
-
-## Paragraph 1
-
-This is a **story** about _nothing_!
-
-Yes, yes, yes
-
-No, no, no
